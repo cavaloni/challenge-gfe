81608911befd080780de386ff4702e76e177029f
Chad Avalon <cavalon@zulily.com>
2023-10-18 12:09:55 -0700

    Initial commit. Need cleanup + more

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,96 @@
-import Image from 'next/image'
+'use client'
+import React from 'react'
+import { useState, useEffect, useRef, useCallback } from 'react';
 
-export default function Home() {
+
+const Home = () => {
+  const currencyList = ['USD', 'EUR', 'GBP', 'CNY', 'JPY'];
+
+  const [currentWUCPrice, updateCurrentWUCPrice] = useState(0);
+  const [previousWUCPrice, updatePreviousWUCPrice] = useState(0);
+  const [fetching, updateFetching] = useState(false);
+  const [currency, updateCurrency] = useState(currencyList[0]);
+  const [amount, updateAmount] = useState(0)
+  const [totalAmount, updateTotalAmount] = useState(0)
+
+  const intervalFetcher = useRef();
+
+  const getCurrentVal = async (currentCurrency, currentAmount) => {
+	if (fetching) return
+	console.log(currentCurrency)
+    updateFetching(true);
+    const url = `https://api.frontendeval.com/fake/crypto/${currentCurrency.toLowerCase()}`
+    const res = await fetch(url);
+    let data;
+    try {
+      data = await res.json()
+    } catch (e) {
+      console.log(e)
+    }
+	const curVal = data.value
+    console.log('something', typeof curVal, typeof currentAmount, currentAmount, curVal)
+    const totalVal = currentAmount * curVal
+    console.log(currentAmount, curVal, totalVal)
+    updateTotalAmount(totalVal)
+    updatePreviousWUCPrice(currentWUCPrice);
+    updateCurrentWUCPrice(curVal);
+    updateFetching(false);
+	console.count('getCurrentVal')
+  }
+
+  const setIntervalFetcher = (currentCurrency, currentAmount) => {
+    if (intervalFetcher.current) {
+      clearInterval(intervalFetcher.current);
+    }
+	try {
+		getCurrentVal(currentCurrency, currentAmount);
+	  } catch (e) {
+		console.log(e)
+	  }
+    if (!fetching && currency) {
+      intervalFetcher.current = setInterval(() => {
+        if (!fetching) {
+          try {
+            getCurrentVal(currentCurrency, currentAmount);
+          } catch (e) {
+            console.log(e)
+          }
+        }
+      }, 5000);
+    }
+  }
+
+  useEffect(() => {
+	  setIntervalFetcher(currency, amount)
+  },[])
+  
+  const handleCurrencyChange = ({ target: { value } }) => {
+	console.log('--------------------')
+    updateCurrency(value);
+	setIntervalFetcher(value, amount)
+  };
+
+  const handleAmountChange = ({ target: { value }}) => {
+    updateAmount(value)
+	setIntervalFetcher(currency, value)
+  }
+  console.log(currency)
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div>
+      <input value={amount} type={"number"} onChange={e => handleAmountChange(e)}/>
+      <select
+        value={currency}
+        onChange={(e) => handleCurrencyChange(e)}
+      >
+        {currencyList.map((cur) => (
+          <option value={cur} key={cur}>
+            {cur}
+          </option>
+        ))}
+      </select>
+      <div>{totalAmount}</div>
+    </div>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Home
